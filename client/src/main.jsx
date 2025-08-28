9a9c577ab68861599f8d81f88867bfaf39d2bea8
Sonu Kumar <sonunigam0382002@gmail.com>
2025-08-28 17:21:07 +0530

    chore: setup React with Tailwind CSS and DaisyUI

---
+++ client/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/authentication/Login.jsx";
+import Signup from "./pages/authentication/Signup.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+     <App />
+    <RouterProvider router={router}/>
+     
+  </StrictMode>
+);
